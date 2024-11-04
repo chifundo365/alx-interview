657fe87bf98d7814057822093e473419afbdcf8c
= <=>
2024-11-04 13:21:27 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,18 +6,10 @@ def pascal_triangle(n):
     """ Returns Pascal's Triangle """
     triangle = []
 
-    for row in range(n):
-        if row == 0:
-            triangle.append([1])
-        elif row == 1:
-            triangle.append([1, 1])
-        else:
-            last_row = triangle[-1]
-            new_row = []
-            new_row.append(1)
+    for i in range(n):
+        row = [1] * (i + 1)
 
-            for i in range(len(last_row) - 1):
-                new_row.append(last_row[i] + last_row[i + 1])
-            new_row.append(1)
-            triangle.append(new_row)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
     return triangle
